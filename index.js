890d785f236c3bb3764d34e91234fe0dca669ca8
Halilu Sheidu <85502322+Abduljelil32@users.noreply.github.com>
2022-06-18 09:43:45 +0000

    adding /home route

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require ('http');
 
 var server = http.createServer(function(req,res){
 
-  if (req.url == '/'){
+  if (req.url == '/' || req.url == "/home"){
 
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(`
